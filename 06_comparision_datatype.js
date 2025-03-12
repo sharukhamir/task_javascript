6a3da73be18ff5d5e26339672cf11614e9fecce0
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-12 07:11:52 +0000

    comarision datatype

---
+++ 06_comparision_datatype.js
@@ -0,0 +1,35 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// these type of code can be the problem during project
+/*
+console.log("2" > 1)
+console.log(null > 0)
+console.log(null < 0);
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null == 0)
+*/
+// these type of code can be the problem during project
+/*
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+*/
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
+
+
+
+
+
