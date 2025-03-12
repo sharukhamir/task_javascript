f7107ba008062d244187fdf2f42878d0dbb5eda2
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-12 06:31:47 +0000

    datatype basic

---
+++ datatype_conversion.js
@@ -0,0 +1,9 @@
+let score = 3244;
+//let jersy_name = shah10;
+//console.log(typeof(score));
+//console.log(typeof(jersy_name));
+let valueinNumber = score;
+console.log(valueinNumber);
+//let valueinNumber = Number(abc);
+//console.log(typeof(abc))
+//console.log(abc)
