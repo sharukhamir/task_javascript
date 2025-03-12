ecd01f9e883706390f8d1fc500a7d5a3b1ef35b5
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-12 07:39:07 +0000

    change file name

---
+++ 09_string.js
@@ -0,0 +1,30 @@
+//const accountId = 12345
+//const = accountId = 42325
+//let accountName = "Sharukh Amir"
+//var accountEmail = "sharukhamir05@gmail.com"
+//accountEmail = "sharukharain10@gmail.com"
+//accountBank = "Hyderabad"
+//console.log(accountBank);
+//let name = "sharukh"
+console.log("Sharukh")
+//console.log("Amir")
+
+//console.table([accountId, accountName, accountEmail, accountBank])
+//"use strict" // treat all JS code as new version
+//alert("hello") we are using node Js not browser
+//datatypes
+//console.log(3+3) 
+//console.log("Sharukh")
+//let name = "Sharukh Amir" // string
+//let age = 18 // integer
+// number length 2 -> 53
+// bigint
+// string=> int
+// bolean=> true/false
+// null
+// undefined
+// symbol
+// typeof
+//let islogin = true  // boolean 
+//console.table([name, age])
+//console.log(typeof(name))
