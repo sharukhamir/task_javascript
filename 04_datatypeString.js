462030488fd0e80d3eea0c11a9d7b326cb8b3846
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-12 07:44:11 +0000

    changes

--- 04_datatypeString.js
+++ 04_datatypeString.js
@@ -31,3 +31,23 @@ console.log(+"");
  console.log(gameCounter2)
 
 
+
+//console.table([accountId, accountName, accountEmail, accountBank])
+//"use strict" // treat all JS code as new version
+//alert("hello") we are using node Js not browser
+//datatypes
+//console.log(3+3) 
+//console.log("Sharukh")
+//let name = "Sharukh Amir" // string
+//let age = 18 // integer
+// number length 2 -> 53
+// bigint
+// string=> int
+// bolean=> true/false
+// null
+// undefined
+// symbol
+// typeof
+//let islogin = true  // boolean 
+//console.table([name, age])
+//console.log(typeof(name))
