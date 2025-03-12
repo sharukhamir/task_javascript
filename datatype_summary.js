5f226fd48d0830913596ab00080725c7b38fd7b2
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-12 07:14:26 +0000

    datatype summary

---
+++ datatype_summary.js
@@ -0,0 +1,30 @@
+// Datatype
+// two types
+
+// Primitive Datatype
+// 7 types
+// String, Number, Boolean, null, undefined, symbol, BigInt
+let name = "Sharukh"
+let score = 212
+let totalNum = 85.5
+const value = 3.14
+let userEmail
+let islogedin = false
+const outsideTem = null
+const id = Symbol('123')
+//const anotherId = symbol('123')
+//console.log(id === anotherId)
+let bigNumber = 12837474755757n
+
+
+//Reference (Non Primitive Datatype)
+// Array, Object, Function
+let heroes = ["ShahRukh Khan" , "Salman Khan", "Aamir Khan"]
+/*let myobj{
+    name: "Sharukh Amir"
+    age: 24
+}*/
+/*const myFunction(){
+    console.log("Hello World")
+}*/
+console.log(typeof(bigNumber))
