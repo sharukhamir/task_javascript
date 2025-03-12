62805a4c3bc3b34f8e1de95d0034394845008150
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-12 07:38:08 +0000

    change file name

---
+++ 03_datatype_conversion.js
@@ -0,0 +1,18 @@
+let score = 3244;
+//let jersy_name = shah10;
+//console.log(typeof(score));
+//console.log(typeof(jersy_name));
+let valueinNumber = score;
+console.log(valueinNumber);
+//let valueinNumber = Number(abc);
+//console.log(typeof(abc))
+//console.log(abc)
+ 
+let islogin = 1
+ let booleanislogin = Boolean(islogin)
+ console.log(booleanislogin)
+
+ let someNumber = 33
+ let someNumberString = String(someNumber)
+ console.log(someNumberString)
+ console.log(typeof(someNumber))
