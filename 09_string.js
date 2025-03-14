cd4ef2ea2148abcf0cee4ae7e79ea95d24cdea50
sharukhamir <77627377+sharukhamir@users.noreply.github.com>
2025-03-14 10:36:41 +0000

    string

--- 09_string.js
+++ 09_string.js
@@ -8,23 +8,56 @@
 //let name = "sharukh"
 console.log("Sharukh")
 //console.log("Amir")
+const fname = "Sharukh"
+const lname = "Amir"
+//console.log(fname + lname)
+console.log(`My first name is ${fname} and my last name is ${lname}`);
+console.log(`My first name is ${fname} and my last name is ${lname}`);
 
-//console.table([accountId, accountName, accountEmail, accountBank])
-//"use strict" // treat all JS code as new version
-//alert("hello") we are using node Js not browser
-//datatypes
-//console.log(3+3) 
-//console.log("Sharukh")
-//let name = "Sharukh Amir" // string
-//let age = 18 // integer
-// number length 2 -> 53
-// bigint
-// string=> int
-// bolean=> true/false
-// null
-// undefined
-// symbol
-// typeof
-//let islogin = true  // boolean 
-//console.table([name, age])
-//console.log(typeof(name))
+const gameName = new String('SharukhAmirArian')
+/*console.log(gameName)
+console.log(gameName[0])
+console.log(gameName,__proto__)
+console.log(gameName.length);
+console.log(gameName.toUpperCase());*/
+//console.log(gameName.charAT(2));
+//console.log(gameName.indexof('t'));
+
+//const newString = gameName.substring(0, 4)
+//console.log(newString);
+ 
+//const anotherString = gameName.slice(-8, 4)
+//console.log(anotherString); 
+
+//trim documentation on mdn site
+
+const newStringOne = "  Sharukh  " 
+console.log(newStringOne);
+console.log(newStringOne.trim);
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '_')); 
+//console.log(url.include('Sharukh'));
+
+//search split function
+//and search other functions of String
+
+console.log(gameName.split('_'));
+
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+//console.log(balnace.toString().length);
+//console.log(balnace.toFixed(2))
+
+//search use of precision
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3))
+ 
+//const hundreds = 100000
+//console.log(hundreds.toLocalString('en-pkr'))
+
+ //min max value all methods search and use
